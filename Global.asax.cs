970e8396fc3726f7c2af3b219dd67cfccfa7a152
Jeremy Gray <jegra@microsoft.com>
2021-03-03 09:15:56 -0500

    Add project files.

---
+++ Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebFormsBlobStorage
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+
+            Logger.AddLog(ex, string.Empty);
+            if (ex.InnerException != null) Logger.AddLog(ex.InnerException, string.Empty);
+            this.Response.Redirect("Error.aspx", true);
+        }
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,70 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebFormsBlobStorage
+{
+    public static class Logger
+    {
+
+        async static Task StreamToCloudStorageAsync(string accountName, string containerName, Stream sourceStream, string destinationFileName, bool overwrite = true, Dictionary<string, string> destinationFileMetadata = null)
+        {
+            // Construct the blob container endpoint from the arguments.
+            string containerEndpoint = string.Format("https://{0}.blob.core.windows.net/{1}",
+                                                    accountName,
+                                                    containerName);
+
+            // Get a credential and create a client object for the blob container.
+            BlobContainerClient containerClient = new BlobContainerClient(new Uri(containerEndpoint), new DefaultAzureCredential());
+
+
+            try
+            {
+                // Create the container if it does not exist.
+                await containerClient.CreateIfNotExistsAsync();
+
+                var blobClient = containerClient.GetBlobClient(destinationFileName);
+
+                // Upload text to a new block blob.
+                var blob = await blobClient.UploadAsync(sourceStream, overwrite);
+                //await containerClient.UploadBlobAsync(destinationFileName, sourceStream);      
+
+                if (null != destinationFileMetadata)
+                {
+                    blobClient.SetMetadata(destinationFileMetadata);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(containerEndpoint);
+                Console.WriteLine(destinationFileName);
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+
+        internal static void AddLog(Exception ex, string empty)
+        {
+            byte[] byteArray = Encoding.ASCII.GetBytes(ex.Message);
+            string fileName = Guid.NewGuid().ToString() + ".txt";
+            string storageAccountName = ConfigurationManager.AppSettings["storageAccountName"].ToString();
+            string storageContainerName = ConfigurationManager.AppSettings["storageContainerName"].ToString();
+
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            {
+                
+                StreamToCloudStorageAsync(storageAccountName, storageContainerName, stream, fileName).GetAwaiter().GetResult();
+
+            }
+        }
+    }
+}
